9fc1714c7471d651cf553d9b0889af6717e0d524
Jing Chai <chai.j@ieee.org>
2017-10-25 15:31:39 -0400

    Setup components and initial layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoComponent } from './video/video.component';
+import { VideoEmbedderComponent } from './video-embedder/video-embedder.component';
+import { VideoStatsComponent } from './video-stats/video-stats.component';
+import { StatsFilterComponent } from './stats-filter/stats-filter.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoListComponent,
+    VideoComponent,
+    VideoEmbedderComponent,
+    VideoStatsComponent,
+    StatsFilterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
