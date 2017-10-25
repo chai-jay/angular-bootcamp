4bfb38871e078b24222cdb56111b6e1910518b17
Jing Chai <chai.j@ieee.org>
2017-10-25 15:31:58 -0400

    Setup components and initial layout.

---
+++ src/app/stats-filter/stats-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-filter',
+  templateUrl: './stats-filter.component.html',
+  styleUrls: ['./stats-filter.component.css']
+})
+export class StatsFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/video-stats/video-stats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-stats',
+  templateUrl: './video-stats.component.html',
+  styleUrls: ['./video-stats.component.css']
+})
+export class VideoStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
